a1bd3a76d5b9603dc6feac572598541ec58c05f8
kvitadmin <kutukov@list.ru>
2021-08-17 07:54:55 +0300

    Temp commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    bool EndTheGame = false;
+    std::string FishName, ContinueGame, TmpString;
+    std::ifstream RiverFile;
+    std::ofstream BasketFile;
+    do{
+        std::cout << "Enter the fish: ";
+        std::cin >> FishName;
+        RiverFile.open("../river.txt");
+        while(!RiverFile.eof()){
+            RiverFile >> TmpString;
+            if(TmpString == FishName){
+                std::cout << " You have caught a fish! " << std::endl;
+                BasketFile.open("../basket.txt",std::ios::app);
+                BasketFile << FishName << std::endl;
+                BasketFile.close();
+            }
+        }
+        RiverFile.close();
+        std::cout << "Continue the game? (Yes/No): ";
+        std::cin >> ContinueGame;
+        if(ContinueGame == "Yes") EndTheGame = false;
+        else EndTheGame = true;
+    }while(!EndTheGame);
+    std::cout << "Your basket: " << std::endl;
+    std::ifstream FileToPrint("../basket.txt");
+    while (!FileToPrint.eof()){
+        FileToPrint >> TmpString;
+        std::cout << TmpString << std::endl;
+    }
+    FileToPrint.close();
     return 0;
 }
